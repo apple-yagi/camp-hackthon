e8d2d30eb08501d57b2509d383d56b30a0b8b0ac
apple-yagi <applesryu@gmail.com>
2020-09-20 22:40:01 +0900

    marker iconをbeachflagに変更

--- src/components/VGoogleMap.vue
+++ src/components/VGoogleMap.vue
@@ -6,24 +6,29 @@
       map-type-id="terrain"
       style="width: 500px; height: 300px"
     >
-      <GmapMarker
-        :position="marker.position"
-        :clickable="true"
-        :draggable="true"
-      />
+      <GmapMarker :icon="flag" :position="marker.position" :clickable="true" @click="show" />
     </GmapMap>
   </div>
 </template>
 
 <script lang="ts">
-import Vue, { PropType } from 'vue';
-import { GoogleMapsMarker } from '@/interfaces/google-maps-marker';
+import Vue, { PropType } from "vue";
+import { GoogleMapsMarker } from "@/interfaces/google-maps-marker";
 
 export default Vue.extend({
   props: {
     marker: {
       type: Object as PropType<GoogleMapsMarker>,
       required: true,
+    },
+  },
+  data: () => ({
+    flag:
+      "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+  }),
+  methods: {
+    show() {
+      alert(this.marker);
     },
   },
 });
