119e5a7093fe131e96f65ad4597ebecb748e933d
apple-yagi <applesryu@gmail.com>
2020-09-22 00:28:04 +0900

    add rules

---
+++ src/rules/index.ts
@@ -0,0 +1,16 @@
+export const usernameRules = [
+  (v: string) => !!v || 'ユーザー名は必須項目です',
+  (v: string) =>
+    (v && v.length >= 4) || 'ユーザー名は4文字以上入力してください',
+];
+
+export const passwordRules = [
+  (v: string) => !!v || 'パスワードは必須項目です',
+  (v: string) =>
+    (v && v.length >= 6) || 'パスワードは6文字以上入力してください',
+];
+
+export const emailRules = [
+  (v: string) => !!v || 'E-mail is required',
+  (v: string) => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+];
