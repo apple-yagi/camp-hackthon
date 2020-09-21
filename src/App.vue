cdfc908185c3e994adc9cbecc6d13148dda55431
apple-yagi <applesryu@gmail.com>
2020-09-22 00:28:34 +0900

    add VSignupForm

--- src/App.vue
+++ src/App.vue
@@ -9,20 +9,21 @@
 
       <v-spacer />
 
-      <v-btn
-        v-if="uid"
-        class="mr-2"
-        @click.stop="postFormDialog = true"
-        outlined
-      >
+      <v-btn v-if="uid" class="mr-2" @click.stop="postFormDialog = true" outlined>
         <span class="hidden-sm-and-down">投稿</span>
         <v-icon>mdi-telegram</v-icon>
       </v-btn>
 
-      <v-btn v-else class="mr-2" @click.stop="loginFormDialog = true" outlined>
-        <span class="hidden-sm-and-down">ログイン</span>
-        <v-icon>mdi-login</v-icon>
-      </v-btn>
+      <div v-else>
+        <v-btn class="mr-2" @click.stop="loginFormDialog = true" outlined>
+          <span class="hidden-sm-and-down">ログイン</span>
+          <v-icon>mdi-login</v-icon>
+        </v-btn>
+        <v-btn class="mr-2" @click.stop="signupFormDialog = true" outlined>
+          <span class="hidden-sm-and-down">サインアップ</span>
+          <v-icon>mdi-account-check</v-icon>
+        </v-btn>
+      </div>
 
       <v-custom-dialog
         :dialog="postFormDialog"
@@ -39,6 +40,14 @@
       >
         <v-login-form @close-dialog="loginFormDialog = false" />
       </v-custom-dialog>
+
+      <v-custom-dialog
+        :dialog="signupFormDialog"
+        :title="signupFormTitle"
+        @close-dialog="signupFormDialog = false"
+      >
+        <v-signup-form @close-dialog="signupFormDialog = false" />
+      </v-custom-dialog>
     </v-app-bar>
 
     <v-main class="base-setting">
@@ -48,28 +57,32 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import VCustomDialog from '@/components/utils/VCustomDialog.vue';
-import VPostForm from '@/components/VPostForm.vue';
-import VLoginForm from '@/components/VLoginForm.vue';
-import { mapState } from 'vuex';
-import { AuthState } from './interfaces/store';
+import Vue from "vue";
+import VCustomDialog from "@/components/utils/VCustomDialog.vue";
+import VPostForm from "@/components/VPostForm.vue";
+import VLoginForm from "@/components/VLoginForm.vue";
+import VSignupForm from "@/components/VSignupForm.vue";
+import { mapState } from "vuex";
+import { AuthState } from "./interfaces/store";
 
 export default Vue.extend({
-  name: 'App',
+  name: "App",
   components: {
     VCustomDialog,
     VPostForm,
     VLoginForm,
+    VSignupForm,
   },
   data: () => ({
     postFormDialog: false,
     loginFormDialog: false,
-    postFormTitle: 'Post Form',
-    loginFormTitle: 'Login From',
+    signupFormDialog: false,
+    postFormTitle: "Post Form",
+    loginFormTitle: "Login Form",
+    signupFormTitle: "Signup Form",
   }),
   computed: {
-    ...mapState('auth', {
+    ...mapState("auth", {
       uid: (state: any) => state.uid,
     }),
   },
---
+++ src/components/VSignupForm.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-card>
+    <v-card-text>
+      <v-row class="pt-5" justify="center">
+        <v-col cols="10" v-show="error" sm="8">
+          <v-alert type="error">{{ error }}</v-alert>
+        </v-col>
+        <v-col cols="10" sm="8">
+          <v-text-field v-model="signupInfo.username" label="username" outlined></v-text-field>
+        </v-col>
+        <v-col cols="10" sm="8">
+          <v-text-field
+            v-model="signupInfo.password"
+            label="password"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="showPassword ? 'text' : 'password'"
+            @click:append="showPassword = !showPassword"
+            outlined
+          ></v-text-field>
+        </v-col>
+        <v-col cols="10" sm="8">
+          <v-text-field
+            v-model="signupInfo.confirm_password"
+            label="password"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="showPassword ? 'text' : 'password'"
+            @click:append="showPassword = !showPassword"
+            outlined
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-card-text>
+    <v-card-actions>
+      <v-layout justify-end>
+        <v-btn class="mr-3">
+          <span>Signup</span>
+          <v-icon>mdi-account-check</v-icon>
+        </v-btn>
+        <v-btn @click="close" class="mr-3">
+          <span>Cancel</span>
+        </v-btn>
+      </v-layout>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { usernameRules, passwordRules } from "@/rules";
+import { SignupInfo } from "@/interfaces/signup-info";
+
+export default Vue.extend({
+  data: () => ({
+    signupInfo: {} as SignupInfo,
+    usernameRules,
+    passwordRules,
+    showPassword: false,
+  }),
+  methods: {
+    close() {
+      this.$emit("close-dialog");
+    },
+  },
+});
+</script>
