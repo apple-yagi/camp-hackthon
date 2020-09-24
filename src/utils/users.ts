7af1fecc65664fa16234ac62353d7832f4bbeeb4
apple-yagi <applesryu@gmail.com>
2020-09-24 15:12:10 +0900

    update

--- src/utils/users.ts
+++ src/utils/users.ts
@@ -1,3 +1,4 @@
+import { Insect } from '@/interfaces/insects';
 import { User } from '@/interfaces/user';
 import axios, { AxiosResponse } from 'axios';
 const BaseUrl = process.env.VUE_APP_BASE_URL || 'http://localhost:3000/';
@@ -11,6 +12,17 @@ export default {
       return Promise.resolve(res.data);
     } catch (error) {
       return Promise.reject('不明なユーザ');
+    }
+  },
+
+  async loadInsects(userId: number): Promise<Insect[]> {
+    try {
+      const res: AxiosResponse<Insect[]> = await axios.get(
+        `${BaseUrl}users/${userId}/insects`
+      );
+      return Promise.resolve(res.data);
+    } catch (error) {
+      return Promise.reject('データの取得に失敗しました');
     }
   },
 };
